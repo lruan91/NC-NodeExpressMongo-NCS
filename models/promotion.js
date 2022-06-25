2777136ed0831880988b4c7e4f3d39cc2cd82c90
Lynnea <lruan.dev@gmail.com>
2022-06-25 12:26:09 -0400

    Week 2 Task 3: Updating the Promtions and Partners routers

--- models/promotion.js
+++ models/promotion.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
 const promotionSchema = new Schema({
 	name: {
 		type: String,
@@ -19,7 +22,7 @@ const promotionSchema = new Schema({
     type: Currency,
     required: true,
     min: 0
- },
+},
   description: {
     type: String,
     required: true
