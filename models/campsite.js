d1a8748812c5bbe7224fd6f1a28377ac40c7b1e1
Lynnea <lruan.dev@gmail.com>
2022-06-25 00:21:51 -0400

    Express REST API with MongoDB and Mongoose Part 1

---
+++ models/campsite.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+// Making a shorthand
+const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const commentSchema = new Schema({
+  rating: {
+    type: Number,
+    min: 1, 
+    max: 5,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+// new object named campsiteSchema
+// This document will require a name and it will be unique. The description will be a type string and required
+const campsiteSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  elevation: {
+    type: Number,
+    required: true
+  },
+  cost: {
+    type: Currency,
+    required: true,
+    min: 0
+  },
+  featured: {
+    type: Boolean,
+    default: false
+  },
+  comments: [commentSchema]
+}, {
+  timestamps: true
+});
+
+// creates a model named Campsite
+const Campsite = mongoose.model('Campsite', campsiteSchema);
+
+module.exports = Campsite;
